02c3f957f599f09d1723f6ef31e7495bae1183f7
WEBundleXYZ <stefg@webundlexyzsmbp.mynetworksettings.com>
2022-01-23 11:09:30 -0500

    Node Express Ejs MongoDB - Project Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+#### Node-Express-MongoDB
+
+[1 - Project Setup ](../../tree//)
+
+[2 - Empty ](../../tree//)
+
+[3 - Empty ](../../tree//)
---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('view', 'views');
+
+app.listen(3000);
